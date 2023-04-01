f511cf9cc26a8334e6d01b1d830ff143ab05f13e
Kevin Jiang <kevin.jiang@columbia.edu>
2023-03-31 22:48:47 -0400

    Delete make_archive.sh

--- make_archive.sh
+++
@@ -1,8 +0,0 @@
-# Create a temporary directory
-mkdir all_mp3s
-
-# Find and copy all .mp3 files to the temporary directory while preserving the filename
-find . -type f -iname "*.mp3" -exec bash -c 'cp "{}" "all_mp3s/$(basename "{}")"' \;
-
-# Create a zip archive containing the files in the temporary directory
-zip -r all_mp3s.zip all_mp3s
